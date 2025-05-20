8b464ebfe1f7016e8a1e734f249389067cc77369
Olwethu Mukona <siyaolwethu433@gmail.com>
2025-05-20 09:34:28 +0200

    Update Question4.kt

--- app/src/main/java/vcmsa/ci/flashcardquizapplication/Question4.kt
+++ app/src/main/java/vcmsa/ci/flashcardquizapplication/Question4.kt
@@ -15,18 +15,20 @@ class Question4 : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_question4)
         //----------------------------------------------------------------------------------
+        //(Kotlinlang, 2024)
         val btnTrue4 = findViewById<Button>(R.id.btnTrue4)
         val btnFalse4 = findViewById<Button>(R.id.btnFalse4)
         val btnExit4 = findViewById<Button>(R.id.btnExit4)
         //Display a question and include two buttons for the answer options “True” and “False”
+        //(Donn Felker, 2017)
         val answers = arrayOf(true, false, true, false, false)  //Correct Answer
         val correctAns = answers[3]
         val score = intent.getIntExtra("score", 0)
 
-        //After you clicking the "answer," the following feedback for each answer will be “Correct!” or “Incorrect”
+        //After you clicking the "answer" the following feedback for each answer will be “Correct!” or “Incorrect”
         btnTrue4.setOnClickListener {
             if (!correctAns) {
-                Toast.makeText(this,"Incorrect!",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this,"Incorrect!",Toast.LENGTH_SHORT).show() /* (Android Developer, 2024) */
             } else {
                 // Score stays the same because the answer is incorrect
                 Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
